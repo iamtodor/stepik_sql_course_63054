8a3a7121ac683c6ab75acf21a3f03e528366bb38
Denis <64330549+denisgribenyuk@users.noreply.github.com>
2022-09-18 14:36:43 +0300

    Finish 3.9, Add 3.4, 3.5, 4.1 module (#18)
    Co-authored-by: d.gribenyuk <o94#*kcom92L>

--- solutions.sql
+++ solutions.sql
@@ -741,3 +741,329 @@ join program p on p.program_id=ps.program_id
 where name_subject in ('Математика', 'Информатика')
 group by name_program
 having count(*)=2
+
+3.3.10
+SELECT p.name_program, e.name_enrollee, SUM(es.result) AS itog
+FROM program_subject ps
+    INNER JOIN program p USING(program_id)
+    INNER JOIN program_enrollee pe USING(program_id)
+    INNER JOIN enrollee e USING(enrollee_id)
+    INNER JOIN enrollee_subject es ON es.subject_id = ps.subject_id AND
+                                  es.enrollee_id = pe.enrollee_id
+GROUP BY 1, 2
+ORDER BY 1, 3 DESC;
+
+3.3.11
+SELECT name_program, name_enrollee
+FROM enrollee
+     JOIN program_enrollee USING(enrollee_id)
+     JOIN program USING(program_id)
+     JOIN program_subject USING(program_id)
+     JOIN subject USING(subject_id)
+     JOIN enrollee_subject USING(subject_id)
+WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id and result < min_result
+ORDER BY 1, 2
+
+3.4.1
+CREATE TABLE applicant
+SELECT program_id, enrollee.enrollee_id, SUM(result) AS itog
+FROM enrollee
+     JOIN program_enrollee USING(enrollee_id)
+     JOIN program USING(program_id)
+     JOIN program_subject USING(program_id)
+     JOIN subject USING(subject_id)
+     JOIN enrollee_subject USING(subject_id)
+WHERE enrollee_subject.enrollee_id = enrollee.enrollee_id
+GROUP BY program_id, enrollee_id
+ORDER BY program_id, itog DESC;
+
+3.4.2
+DELETE FROM applicant
+USING
+  applicant
+  JOIN (
+    SELECT program_enrollee.program_id, program_enrollee.enrollee_id 
+    FROM program
+    JOIN program_subject  USING(program_id)
+    JOIN program_enrollee USING(program_id)
+    JOIN enrollee_subject ON 
+    enrollee_subject.enrollee_id = program_enrollee.enrollee_id AND
+    enrollee_subject.subject_id = program_subject.subject_id
+    WHERE result < min_result
+ ) AS t
+ ON applicant.program_id = t.program_id AND
+    applicant.enrollee_id = t.enrollee_id
+
+3.4.3
+UPDATE applicant JOIN (
+    SELECT enrollee_id, IFNULL(SUM(bonus), 0) AS Бонус FROM enrollee_achievement
+    LEFT JOIN achievement USING(achievement_id)
+    GROUP BY enrollee_id 
+    ) AS t USING(enrollee_id)
+SET itog = itog + Бонус;
+
+3.4.4
+CREATE TABLE applicant_order
+SELECT * FROM applicant
+ORDER BY 1, 3 DESC;
+DROP TABLE applicant
+
+3.4.5
+ALTER TABLE applicant_order ADD
+str_id int FIRST
+
+3.4.6
+SET @row_num := 1;
+SET @num_pr := 0;
+UPDATE applicant_order
+    SET str_id = IF(program_id = @num_pr, @row_num := @row_num + 1, @row_num := 1 AND @num_pr := @num_pr + 1);
+
+3.4.7
+CREATE TABLE student
+SELECT name_program, name_enrollee, itog FROM enrollee
+	JOIN applicant_order USING (enrollee_id)
+	JOIN program USING (program_id)
+WHERE str_id<=plan
+ORDER BY name_program, itog DESC;
+
+3.5.1
+SELECT CONCAT(LEFT(CONCAT(module_id, ' ', module_name), 16), '...') Модуль,
+       CONCAT(LEFT(CONCAT(module_id, '.', lesson_position, ' ', lesson_name), 16), '...') Урок,
+       CONCAT(module_id, '.', lesson_position, '.', step_position, ' ', step_name) Шаг
+  FROM module
+       INNER JOIN lesson USING(module_id)
+       INNER JOIN step   USING(lesson_id)
+ WHERE step_name LIKE '%ложенн% запрос%'
+ ORDER BY module_id, lesson_id, step_id;
+
+3.5.2
+INSERT INTO step_keyword
+SELECT step.step_id, keyword.keyword_id 
+FROM 
+    keyword
+    CROSS JOIN step
+WHERE step.step_name REGEXP CONCAT(' ', CONCAT(keyword.keyword_name, '\\b'))
+GROUP BY step.step_id, keyword.keyword_id
+ORDER BY keyword.keyword_id;
+
+3.5.3
+SELECT 
+    concat(module_id,'.',lesson_position,
+           IF(step_position < 10, ".0","."),
+           step_position," ",step_name) AS Шаг
+FROM
+   step
+   JOIN lesson USING(lesson_id)
+   JOIN module USING(module_id)
+   JOIN step_keyword USING (step_id)
+   JOIN keyword USING(keyword_id)
+WHERE keyword_name = 'MAX' OR keyword_name ='AVG'
+GROUP BY ШАГ
+HAVING COUNT(*) = 2
+ORDER BY 1;
+
+3.5.4
+SELECT
+    rate_group Группа, 
+    CASE rate_group
+        WHEN 'I'   THEN 'от 0 до 10'
+        WHEN 'II'  THEN 'от 11 до 15'
+        WHEN 'III' THEN 'от 16 до 27'
+        ELSE 'больше 27'
+    END Интервал,
+    COUNT(*) Количество
+FROM
+(
+    SELECT 
+        CASE
+            WHEN COUNT(DISTINCT step_id) <= 10 THEN 'I'
+            WHEN COUNT(DISTINCT step_id) <= 15 THEN 'II'
+            WHEN COUNT(DISTINCT step_id) <= 27 THEN 'III'
+            ELSE 'IV'
+        END rate_group
+    FROM step_student
+    WHERE result = 'correct'
+    GROUP BY student_id
+) query_in
+GROUP BY rate_group
+ORDER BY 1;
+
+3.5.5
+WITH table1 (step_name, correct, count) AS (   
+SELECT 
+  step_name, 
+  SUM( IF (result = 'correct' , 1 , 0)) AS s, 
+  COUNT(result) AS c
+  FROM step 
+  JOIN step_student USING (step_id)
+  GROUP BY step_name
+    )
+
+SELECT  step_name AS Шаг, ROUND((correct/count)*100) AS Успешность
+FROM table1
+ORDER BY 2, 1
+
+3.5.6
+WITH get_passed (student_name, pssd)
+    AS
+        (
+           SELECT student_name, COUNT(DISTINCT step_id) AS passed
+           FROM student JOIN step_student USING(student_id)
+           WHERE result = "correct"
+           GROUP BY student_id
+           ORDER BY passed
+         )
+SELECT student_name AS Студент, ROUND(100*pssd/(SELECT COUNT(DISTINCT step_id) FROM step_student)) AS Прогресс,
+    CASE
+        WHEN ROUND(100*pssd/(SELECT COUNT(DISTINCT step_id) FROM step_student)) =  100 THEN "Сертификат с отличием"
+        WHEN ROUND(100*pssd/(SELECT COUNT(DISTINCT step_id) FROM step_student)) >= 80 THEN "Сертификат"
+        ELSE ""
+    END AS Результат
+FROM get_passed
+ORDER BY Прогресс DESC, Студент
+
+3.5.7
+SELECT student_name AS Студент, 
+    CONCAT(LEFT(step_name, 20), '...') AS Шаг, 
+    result AS Результат, 
+    FROM_UNIXTIME(submission_time) AS Дата_отправки,
+    SEC_TO_TIME(submission_time - LAG(submission_time, 1, submission_time) OVER (ORDER BY submission_time)) AS Разница
+FROM student
+    INNER JOIN step_student USING(student_id)
+    INNER JOIN step USING(step_id)
+WHERE student_name = 'student_61'
+ORDER BY Дата_отправки;
+
+3.5.8
+SELECT ROW_NUMBER() OVER (ORDER BY Среднее_время) AS Номер,
+    Урок, Среднее_время
+FROM(
+    SELECT 
+        Урок, ROUND(AVG(difference), 2) AS Среднее_время
+FROM
+     (SELECT student_id,
+             CONCAT(module_id, '.', lesson_position, ' ', lesson_name) AS Урок,
+             SUM((submission_time - attempt_time) / 3600) AS difference
+      FROM module INNER JOIN lesson USING (module_id)
+                  INNER JOIN step USING (lesson_id)
+                  INNER JOIN step_student USING (step_id)
+      WHERE submission_time - attempt_time <= 4 * 3600
+      GROUP BY 1, 2) AS query_1
+GROUP BY 1) AS TA
+order by 3;
+
+3.5.9
+SELECT  module_id AS Модуль, student_name AS Студент, COUNT(DISTINCT step_id) AS Пройдено_шагов ,
+	ROUND(COUNT(DISTINCT step_id) / 
+      MAX(COUNT(DISTINCT step_id)) OVER(PARTITION BY module_id) *100, 1) AS Относительный_рейтинг
+FROM lesson 
+	JOIN step USING (lesson_id)
+	JOIN step_student USING (step_id)
+	JOIN student USING (student_id)
+WHERE result = 'correct'
+GROUP BY module_id, student_name
+ORDER BY 1, 4 DESC, 2
+
+3.5.10
+WITH get_time_lesson(student_name,  lesson, max_submission_time)
+AS(
+    SELECT student_name,  CONCAT(module_id, '.', lesson_position), MAX(submission_time)
+    FROM step_student INNER JOIN step USING (step_id)
+                          INNER JOIN lesson USING (lesson_id)
+                          INNER JOIN student USING(student_id)
+    WHERE  result = 'correct'  
+    GROUP BY 1,2
+    ORDER BY 1),
+get_students(student_name)
+AS(
+    SELECT student_name 
+    FROM get_time_lesson
+    GROUP BY student_name
+    HAVING COUNT(lesson) = 3)
+SELECT student_name as Студент,  
+       lesson as Урок, 
+       FROM_UNIXTIME(max_submission_time) as Макс_время_отправки, 
+       IFNULL(CEIL((max_submission_time - LAG(max_submission_time) OVER (PARTITION BY student_name ORDER BY max_submission_time )) / 86400),'-') as Интервал 
+FROM get_time_lesson
+WHERE student_name in (SELECT * FROM get_students)
+ORDER BY 1,3;
+
+3.5.11
+SET @avg_time := (SELECT CEIL(AVG(submission_time - attempt_time))
+FROM step_student INNER JOIN student USING(student_id)
+WHERE student_name = "student_59" AND (submission_time - attempt_time) < 3600);
+WITH get_stat
+AS
+(
+SELECT student_name, CONCAT(module_id, ".", lesson_position, ".", step_position) AS less, step_id, RANK() OVER (PARTITION BY CONCAT(module_id, ".", lesson_position, ".", step_position) ORDER BY submission_time) AS rang, result, 
+CASE
+    WHEN (submission_time - attempt_time) > 3600 THEN @avg_time
+    ELSE (submission_time - attempt_time)
+END AS qr
+FROM student 
+    INNER JOIN step_student USING(student_id)
+    INNER JOIN step USING(step_id)
+    INNER JOIN lesson USING(lesson_id)
+WHERE student_name = "student_59"
+)
+SELECT student_name AS Студент, less AS Шаг, rang AS Номер_попытки, result AS Результат, SEC_TO_TIME(CEIL(qr)) AS Время_попытки, ROUND((qr / (SUM(qr) OVER (PARTITION BY less ORDER BY less)) * 100), 2) AS Относительное_время
+FROM get_stat
+ORDER BY step_id, 3;
+
+4.1.1
+SELECT beg_range, end_range,
+     ROUND(AVG(price), 2) AS Средняя_цена,
+     SUM(price * amount) AS Стоимость,
+     COUNT(amount) AS Количество
+FROM(
+    SELECT beg_range, end_range, price, amount
+    FROM stat 
+    JOIN book ON beg_range<price AND end_range>price
+    ) table1    
+GROUP BY beg_range, end_range
+ORDER BY 1
+
+4.1.2
+SELECT *
+FROM book
+ORDER BY LENGTH(title)
+
+4.1.3
+DELETE book, supply 
+FROM book, supply
+WHERE book.price LIKE '%.99' 
+    AND supply.price LIKE '%.99';
+
+4.1.4
+SELECT author, title, price, amount,
+    IF(price > 600, ROUND(price * 0.2, 2), '-') AS sale_20, 
+    IF(price > 600, ROUND(price * 0.8, 2),  '-') AS price_sale
+FROM book
+ORDER BY author, title
+
+4.1.5
+SET @avg_price := (SELECT AVG(price) FROM book);
+SELECT author,  
+    SUM(price * amount) AS Стоимость 
+FROM book
+WHERE author in (SELECT author FROM book WHERE price > @avg_price)
+GROUP BY author
+ORDER BY 2 DESC;
+
+4.1.6
+SELECT author AS "Автор", title AS "Название_книги", amount AS "Количество", price AS "Розничная_цена",
+    IF (amount >= 10, 15, 0) AS "Скидка",
+    round (IF (amount >= 10, price * 0.85, price), 2) AS "Оптовая_цена"    
+FROM book
+ORDER BY author, title;
+
+4.1.7
+SELECT author, 
+    COUNT(author) AS Количество_произведений, 
+    MIN(price) AS Минимальная_цена, 
+    SUM(amount) AS Число_книг 
+FROM book
+WHERE amount > 1
+GROUP BY author
+HAVING COUNT(author) > 1
+ORDER BY author;
