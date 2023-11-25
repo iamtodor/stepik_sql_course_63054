81393686450eac906e358e64e85b0b13771d0d9b
iamtodor <todor.ilya@gmail.com>
2023-11-25 18:56:23 +0100

    Update solutions.sql
    remove parentheses

--- solutions.sql
+++ solutions.sql
@@ -353,7 +353,7 @@ on book.genre_id=genre.genre_id
 where amount is null
 
 2.2.4
-select name_city, name_author, date_add('2020-01-01', interval 'FLOOR(RAND()*365)' day) as Дата
+select name_city, name_author, date_add('2020-01-01', interval FLOOR(RAND()*365) day) as Дата
 from author
 cross join city
 order by 1, 3 desc
